95167ec2b81751663322581f514694790923177d
tuttifrutsy <yoratita@hotmail.com>
2019-10-02 00:00:45 -0500

    actualizando db

--- seeds/events.js
+++ seeds/events.js
@@ -8,8 +8,8 @@ const events = [
     description:
       "Artificial Intelligence and how it will be much more disruptive and exciting to our lives, than the personal computer.I will be focusing more on the good than the bad:) Future products and Technologies that will change our daily lives forever much more than the personal computer did",
     scheduledFor: new Date("2019-04-22T14:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b1",
-    speaker: "5d8ed3051237af413b3096eb",
+    stage: "5d8eb64e1c6bb53fb7ccb3c5",
+    speaker: "5d9410627b524b0f4f85bf4b",
     type: "Conferencia"
   },
   {
@@ -18,8 +18,8 @@ const events = [
     description:
       "Roomie Bot es un Robot humanoide que evolucionó desde un prototipo de control del hogar, a un caso de éxito de innovación reconocido por el MIT Technology Review como una de las tecnologías con más potencial para cambiar el mundo, el cual es actualmente utilizado por importantes corporaciones como Bayer y Bimbo para transformar digitalmente su negocio.",
     scheduledFor: new Date("2019-04-22T15:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b1",
-    speaker: "5d8ed3051237af413b3096ec",
+    stage: "5d8eb64e1c6bb53fb7ccb3c1",
+    speaker: "5d9410627b524b0f4f85bf41",
     type: "Conferencia"
   },
   {
@@ -27,33 +27,19 @@ const events = [
     description:
       "Lo hemos leído en libros, visto en las películas y series de televisión; la ciencia ficción nos presenta ideas tan descabelladas que parecen pertenecer a un futuro muy lejano. Sin embargo día a día personas alrededor del mundo están haciéndolas realidad: manipulación genética, carros autónomos, viajes al espacio y la conquista de Marte, por mencionar solo algunas. El futuro está por dejar de ser ficción y es momento de prepararnos para hacer el mejor uso de estas nuevas herramientas y tecnologías. Freddy ha seguido de cerca el desarrollo de varias de estas ideas y nos presentará las diez que ya están aquí.",
     scheduledFor: new Date("2019-04-22T17:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b2",
-    speaker: "5d8ed3051237af413b3096ed",
+    stage: "5d8eb64e1c6bb53fb7ccb3be",
+    speaker: "5d9410627b524b0f4f85bf42",
     type: "Conferencia"
   },
-  {
-    title: "Orquesta Sinfónica Juvenil de Guadalajara",
-    description: "",
-    scheduledFor: new Date("2019-04-22T18:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b3",
-    speaker: "5d856fdc43bd6ac2143fbe8b",
-    type: "Show"
-  },
-  {
-    title: "Talent Match",
-    description: "",
-    scheduleFor: new Date("2019-04-22T23:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b1",
-    type: "Fun"
-  },
+
   {
     title:
       "Creando una real-time Progressive Web Application con Vue.js, Node.js y Socket.io.",
     description:
       "Se analizará la ventaja de usar Vue.js contra los frameworks que le compiten actualmente (Angular.js, React.js), así mismo se desenvuelve el concepto de que es una Progressive Web Application (PWA) y se crea una usando Vue.js para el front-end y Node-js en el back-end haciendo uso de Socket.io para añadir la funcionalidad de real-time bidireccional.",
     scheduledFor: new Date("2019-04-22T16:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b2",
-    speaker: "5d856fdc43bd6ac2143fbe8a",
+    stage: "5d8eb64e1c6bb53fb7ccb3c3",
+    speaker: "5d9410627b524b0f4f85bf44",
     type: "Conferencia",
     land: "DevLand"
   },
@@ -63,9 +49,9 @@ const events = [
     description:
       "En la era actual se hace necesario desarrollar aplicaciones en una forma que permita aprovechar las características de un entorno en la nube. Es muy común partir nuestra aplicación en microservicios, pero ¿cómo diseñamos la arquitectura de dichos microservicios de forma que sean escalables y mantenibles? En esta charla exploraremos estilos arquitectónicos clásicos y modernos, y cómo se aplican a la construcción de microservicios.",
     scheduledFor: new Date("2019-04-22T19:00:00"),
-    stage: "5d8546ab19dcd3bfbece63b7",
+    stage: "5d8eb64e1c6bb53fb7ccb3b6",
     land: "DevLand",
-    speaker: "5d856fdc43bd6ac2143fbe8c",
+    speaker: "5d9410627b524b0f4f85bf45",
     type: "Conferencia"
   },
   {
@@ -74,8 +60,8 @@ const events = [
     description:
       "Es bien sabido que, en cualquier sistema, la performance de sus bases de datos es uno de los mayores cuellos de botella. En este panel, exploramos como utilizar MySQL y su ecosistema open-source para lograr soluciones de alto rendimiento, escalables y económicas.",
     scheduledFor: new Date("2019-04-22T14:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b4",
-    speaker: "5d856fdc43bd6ac2143fbe8d",
+    stage: "5d8eb64e1c6bb53fb7ccb3b2",
+    speaker: "5d9410627b524b0f4f85bf46",
     type: "Conferencia",
     land: "DevLand"
   },
@@ -84,16 +70,16 @@ const events = [
     description:
       "El objetivo de esta charla es dar a conocer los beneficios de integrar métodos como la inteligencia artificial en el área médica, además de mostrar un proyecto con impacto social que la comunidad Ikigai desarrolla.",
     scheduledFor: new Date("2019-04-22T22:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b5",
-    speaker: "5d856fdc43bd6ac2143fbe8f",
+    stage: "5d8eb64e1c6bb53fb7ccb3d6",
+    speaker: "5d9410627b524b0f4f85bf47",
     type: "Conferencia"
   },
   {
     title: "El poder del almacenamiento en el mundo de los videojuegos",
     description: "5d856fdc43bd6ac2143fbe90",
     scheduledFor: new Date("2019-04-22T20:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b6",
-    speaker: "5d856fdc43bd6ac2143fbe91",
+    stage: "5d8eb64e1c6bb53fb7ccb3bf",
+    speaker: "5d9410627b524b0f4f85bf48",
     type: "Conferencia"
   },
   {
@@ -102,7 +88,7 @@ const events = [
       "Los hackers siempre han causado gran curiosidad y misterio para las personas, desde películas de hollywood hasta sus hazañas reales vistas en noticieros y redes sociales. Pero, ¿cómo nace un hacker? ¿cómo inicio en ciberseguridad? Esta charla es para aquellos que quieren iniciar en este mundo pero no saben cómo.",
     scheduledFor: new Date("2019-04-22T20:00:00"),
     stage: "5d8eb64e1c6bb53fb7ccb3b8",
-    speaker: "5d856fdc43bd6ac2143fbe91",
+    speaker: "5d9410627b524b0f4f85bf49",
     type: "Conferencia"
   },
   {
@@ -110,8 +96,8 @@ const events = [
     description:
       "En esta charla abordaremos esas posibilidades y expondremos los beneficios de incluir materiales alternativos en las tecnologías.",
     scheduledFor: new Date("2019-04-22T20:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b9",
-    speaker: "5d856fdc43bd6ac2143fbe92",
+    stage: "5d8eb64e1c6bb53fb7ccb3db",
+    speaker: "5d9410627b524b0f4f85bf4a",
     type: "Conferencia"
   },
   {
@@ -120,8 +106,8 @@ const events = [
     description:
       "Artificial Intelligence and how it will be much more disruptive and exciting to our lives, than the personal computer.I will be focusing more on the good than the bad:) Future products and Technologies that will change our daily lives forever much more than the personal computer did",
     scheduledFor: new Date("2019-04-22T14:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b2",
-    speaker: "5d8ed3051237af413b3096eb",
+    stage: "5d8eb64e1c6bb53fb7ccb3e2",
+    speaker: "5d9410627b524b0f4f85bf4c",
     type: "Lab"
   },
   {
@@ -130,8 +116,8 @@ const events = [
     description:
       "Roomie Bot es un Robot humanoide que evolucionó desde un prototipo de control del hogar, a un caso de éxito de innovación reconocido por el MIT Technology Review como una de las tecnologías con más potencial para cambiar el mundo, el cual es actualmente utilizado por importantes corporaciones como Bayer y Bimbo para transformar digitalmente su negocio.",
     scheduledFor: new Date("2019-04-22T15:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b2",
-    speaker: "5d8ed3051237af413b3096ec",
+    stage: "5d8eb64e1c6bb53fb7ccb3bc",
+    speaker: "5d9410627b524b0f4f85bf4d",
     type: "Lab"
   },
   {
@@ -139,8 +125,8 @@ const events = [
     description:
       "Lo hemos leído en libros, visto en las películas y series de televisión; la ciencia ficción nos presenta ideas tan descabelladas que parecen pertenecer a un futuro muy lejano. Sin embargo día a día personas alrededor del mundo están haciéndolas realidad: manipulación genética, carros autónomos, viajes al espacio y la conquista de Marte, por mencionar solo algunas. El futuro está por dejar de ser ficción y es momento de prepararnos para hacer el mejor uso de estas nuevas herramientas y tecnologías. Freddy ha seguido de cerca el desarrollo de varias de estas ideas y nos presentará las diez que ya están aquí.",
     scheduledFor: new Date("2019-04-22T17:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b3",
-    speaker: "5d84f22813cd7ebc9010f952",
+    stage: "5d8eb64e1c6bb53fb7ccb3b1",
+    speaker: "5d9410627b524b0f4f85bf4e",
     type: "Lab"
   },
   {
@@ -164,8 +150,8 @@ const events = [
     description:
       "Se analizará la ventaja de usar Vue.js contra los frameworks que le compiten actualmente (Angular.js, React.js), así mismo se desenvuelve el concepto de que es una Progressive Web Application (PWA) y se crea una usando Vue.js para el front-end y Node-js en el back-end haciendo uso de Socket.io para añadir la funcionalidad de real-time bidireccional.",
     scheduledFor: new Date("2019-04-22T16:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b2",
-    speaker: "5d856fdc43bd6ac2143fbe8a",
+    stage: "5d8eb64e1c6bb53fb7ccb3d6",
+    speaker: "5d9410627b524b0f4f85bf4f",
     type: "Conferencia",
     land: "DevLand"
   },
@@ -175,9 +161,9 @@ const events = [
     description:
       "En la era actual se hace necesario desarrollar aplicaciones en una forma que permita aprovechar las características de un entorno en la nube. Es muy común partir nuestra aplicación en microservicios, pero ¿cómo diseñamos la arquitectura de dichos microservicios de forma que sean escalables y mantenibles? En esta charla exploraremos estilos arquitectónicos clásicos y modernos, y cómo se aplican a la construcción de microservicios.",
     scheduledFor: new Date("2019-04-22T19:00:00"),
-    stage: "5d8546ab19dcd3bfbece63b7",
+    stage: "5d8eb64e1c6bb53fb7ccb3b2",
     land: "DevLand",
-    speaker: "5d856fdc43bd6ac2143fbe8c",
+    speaker: "5d9410627b524b0f4f85bf50",
     type: "Conferencia"
   },
   {
@@ -186,8 +172,8 @@ const events = [
     description:
       "Es bien sabido que, en cualquier sistema, la performance de sus bases de datos es uno de los mayores cuellos de botella. En este panel, exploramos como utilizar MySQL y su ecosistema open-source para lograr soluciones de alto rendimiento, escalables y económicas.",
     scheduledFor: new Date("2019-04-22T14:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b4",
-    speaker: "5d856fdc43bd6ac2143fbe8d",
+    stage: "5d8eb64e1c6bb53fb7ccb3d6",
+    speaker: "5d9410627b524b0f4f85bf51",
     type: "Conferencia",
     land: "DevLand"
   },
@@ -196,16 +182,16 @@ const events = [
     description:
       "El objetivo de esta charla es dar a conocer los beneficios de integrar métodos como la inteligencia artificial en el área médica, además de mostrar un proyecto con impacto social que la comunidad Ikigai desarrolla.",
     scheduledFor: new Date("2019-04-22T22:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b5",
-    speaker: "5d856fdc43bd6ac2143fbe8f",
+    stage: "5d8eb64e1c6bb53fb7ccb3bf",
+    speaker: "5d9410627b524b0f4f85bf50",
     type: "Conferencia"
   },
   {
     title: "El poder del almacenamiento en el mundo de los videojuegos",
     description: "5d856fdc43bd6ac2143fbe90",
     scheduledFor: new Date("2019-04-22T20:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b6",
-    speaker: "5d856fdc43bd6ac2143fbe91",
+    stage: "5d8eb64e1c6bb53fb7ccb3db",
+    speaker: "5d9410627b524b0f4f85bf4f",
     type: "Conferencia"
   },
   {
@@ -213,8 +199,8 @@ const events = [
     description:
       "Los hackers siempre han causado gran curiosidad y misterio para las personas, desde películas de hollywood hasta sus hazañas reales vistas en noticieros y redes sociales. Pero, ¿cómo nace un hacker? ¿cómo inicio en ciberseguridad? Esta charla es para aquellos que quieren iniciar en este mundo pero no saben cómo.",
     scheduledFor: new Date("2019-04-22T20:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b8",
-    speaker: "5d856fdc43bd6ac2143fbe91",
+    stage: "5d8eb64e1c6bb53fb7ccb3e2",
+    speaker: "5d9410627b524b0f4f85bf4e",
     type: "Conferencia"
   },
   {
@@ -222,8 +208,8 @@ const events = [
     description:
       "En esta charla abordaremos esas posibilidades y expondremos los beneficios de incluir materiales alternativos en las tecnologías.",
     scheduledFor: new Date("2019-04-22T20:00:00"),
-    stage: "5d8eb64e1c6bb53fb7ccb3b9",
-    speaker: "5d856fdc43bd6ac2143fbe92",
+    stage: "5d8eb64e1c6bb53fb7ccb3bc",
+    speaker: "5d9410627b524b0f4f85bf4d",
     type: "Conferencia"
   }
 ];
