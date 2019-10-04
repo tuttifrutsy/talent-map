c085d97dfe551f763705d2b1c6faba3e0906b727
tuttifrutsy <yoratita@hotmail.com>
2019-10-04 12:42:14 -0500

    funcionando login wiiiii

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cors = require("cors");
 const session = require('express-session');
 const MongoStore = require("connect-mongo")(session);
 const passport = require('passport');
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require('./models/User');
 const app = express();
 
 //DB
@@ -49,6 +52,42 @@ app.use(
     })
   })
 );
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      passReqToCallback: true
+    },
+    (req, username, password, next) => {
+      User.findOne({ username }, (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return next(null, false, { message: "Incorrect Username" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return next(null, false, { message: "Incorrect Password" });
+        }
+        return next(null, user);
+      });
+    }
+  )
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 //Routes
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,9 @@
 
 const express = require("express");
 const authRoutes = express.Router();
-
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
-
-
+const bcryptSalt = 10;
 const User = require('../models/User');
 
 authRoutes.post("/signup", (req, res, next) => {
@@ -64,5 +62,39 @@ authRoutes.post("/signup", (req, res, next) => {
     });
   });
 });
+
+authRoutes.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
+      return;
+    }
+
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, err => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+
+
+
+
 
 module.exports = authRoutes;
